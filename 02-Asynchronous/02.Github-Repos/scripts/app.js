22581c1ed5747ff62c64888248292c4917b24604
Aleks Nikolov <tcr.official.world@gmail.com>
2021-10-28 20:44:42 +0300

    ex 02 done

--- 02-Asynchronous/02.Github-Repos/scripts/app.js
+++ 02-Asynchronous/02.Github-Repos/scripts/app.js
@@ -1,3 +1,26 @@
 function loadRepos() {
+	const username = document.getElementById('username').value;
+	const url = `https://api.github.com/users/${username}/repos`;
+
+	fetch(url)
+	.then(response =>{
+		if(!response.ok){
+			throw new Error(response.status + ': ' + response.statusText)
+		}
+		return response.json()} )
+	.then(handleResponse)
+	.catch(err => console.log(err))
 	
+	function handleResponse(data){
+		const list = document.getElementById('repos');
+		list.innerHTML = '';
+		
+		for (const repo of data) {
+			const liEl = document.createElement('li');
+			liEl.innerHTML = `<a href=${repo.html_url}>
+			${repo.full_name}
+		</a>`;
+		list.appendChild(liEl);
+		}
+	};
 }
