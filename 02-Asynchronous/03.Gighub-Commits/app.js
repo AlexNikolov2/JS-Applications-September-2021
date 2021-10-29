3a59d64afe18f85496856bac63d0ab67094e98fa
Aleks Nikolov <tcr.official.world@gmail.com>
2021-10-29 10:01:58 +0300

    03 solved alone by me hahahaha mnogu sam dobarrrr grrrrr

--- 02-Asynchronous/03.Gighub-Commits/app.js
+++ 02-Asynchronous/03.Gighub-Commits/app.js
@@ -1,4 +1,30 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+
+    let list = document.getElementById('commits');
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`
+    fetch(url)
+    .then(response =>{
+		if(!response.ok){
+			throw new Error(response.status + ': ' + response.statusText)
+		}
+		return response.json()} )
+        .then(handleResponse).catch(handleError);
+
+        function handleResponse(data){
+            list.innerHTML = '';
+
+            for (const segment of data) {
+            const li = document.createElement('li');
+            li.textContent = `${segment.commit.author.name}: ${segment.commit.message}`
+            list.appendChild(li);
+            }
+        }
+        function handleError(){
+            list.innerHTML = '';
+            list.innerHTML = '404 not found'
+        }
+
+    //
 }
