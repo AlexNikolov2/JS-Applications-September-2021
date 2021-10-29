7c340a0e7bf08662881bdb9772a4d91c2be87744
Aleks Nikolov <tcr.official.world@gmail.com>
2021-10-29 19:29:09 +0300

    02 done and dusted

--- 02-Asynchronous(exercise)/02.Bus-Schedule/app.js
+++ 02-Asynchronous(exercise)/02.Bus-Schedule/app.js
@@ -1,12 +1,40 @@
 function solve() {
-
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+    const textSpan = document.querySelector('.info');
+    let nextId = 'depot';
+    let current;
     
     function depart() {
+
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${nextId}`;
+
+        fetch(url).then(response =>{
+            if(!response.ok){
+                throw new Error(response.status + ': ' + response.statusText)
+            }
+            return response.json()} )
+            .then(handleResponse).catch(handleError);
+
+            function handleResponse(data){
+                textSpan.innerHTML = `Next stop ${data.name}`
+                nextId = data.next;
+                current = data.name;
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+            }
+            function handleError(){
+                textSpan.textContent = `Error`;
+                departButton.disabled = true;
+                arriveButton.disabled = true;
+            }
         //http://localhost:3030/jsonstore/bus/schedule
     }
 
     function arrive() {
-       
+        textSpan.innerHTML = `Arriving at ${current}`
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
