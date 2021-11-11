21b16531c834db6e2c1615d0a480d8a4f2937fb4
Aleks Nikolov <tcr.official.world@gmail.com>
2021-11-11 21:24:29 +0200

    мумуз

--- 03-Authentication(exercise)/03.Students/app.js
+++ 03-Authentication(exercise)/03.Students/app.js
@@ -1,1 +1,16 @@
-console.log('TODO...');
+let tableBodyElement = document.getElementsByTagName('tbody')[0];
+let submitButton = document.getElementById('submit');
+let formEl = document.getElementById('form');
+let collection = [];
+
+window.onload = await getStudents();
+
+async function getStudents(){
+    
+}
+async function createStudent(){
+
+}
+function generateElements(){
+
+}
