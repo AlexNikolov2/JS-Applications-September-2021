a46872dfb171d03724c709675a887b88b6c06289
Aleks Nikolov <72394391+AlexNikolov2@users.noreply.github.com>
2021-11-18 15:32:27 +0200

    random func

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function balkanLeague(){
     const teams = ['Dinamo Zagreb','Red Star', 'Maribor', 'Olimpija Ljubljana', 'Hajduk Split', 'Rijeka',
                     'Partizan Beolgrade', 'Zrinski', 'Cluj', 'Levski', 'CSKA', 'BEsiktas', 'Galatasaray', 'Fenerbahce']
+    
+    for(let year = 2021; year <= 2051; year++){
+    const randomTeam = teams[Math.floor(Math.random() * teams.length)]
+    console.log(`Champion ${year} : ${randomTeam}`);
+    }
 }
+balkanLeague();
